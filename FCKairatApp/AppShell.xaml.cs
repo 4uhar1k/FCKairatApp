146a8c51210bb7392e0dbbebed87ea27e1a272b2
4uhar1k <vovas0712@gmail.com>
2024-11-20 17:46:43 +0100

    created update button

--- FCKairatApp/AppShell.xaml.cs
+++ FCKairatApp/AppShell.xaml.cs
@@ -1,10 +1,27 @@
-﻿namespace FCKairatApp
+﻿using FCKairatApp.ViewModels;
+namespace FCKairatApp
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+        }
+
+        public void Update(object sender, EventArgs e)
+        {
+            if (Shell.Current.CurrentPage.Title == "Timetable" | Shell.Current.CurrentPage.Title == "Our Team" | Shell.Current.CurrentPage.Title == "TablePage")
+            {
+                Shell.Current.CurrentPage.BindingContext = new GamesNTeamsViewModel();
+            }
+            else if (Shell.Current.CurrentPage.Title == "News")
+            {
+                Shell.Current.CurrentPage.BindingContext = new NewsViewModel();
+            }
+            else if(Shell.Current.CurrentPage.Title == "Your Profile")
+            {
+                Shell.Current.CurrentPage.BindingContext = new ProfileViewModel();
+            }
         }
     }
 }
